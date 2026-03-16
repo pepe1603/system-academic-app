6686d2bf09fb2690027fe9b13f2ba205a6313dc1
pepe1603 <b180022@unach.mx>
2026-03-16 13:54:07 -0600

    Initial commit: Nuxt 4 with Nuxt UI

---
+++ nuxt.config.ts
@@ -0,0 +1,33 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxt/image',
+    '@nuxtjs/google-fonts',
+    '@vueuse/nuxt',
+    'nuxt-aos',
+    'nuxt-lucide-icons'
+  ],
+
+  devtools: {
+    enabled: true
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  routeRules: {
+    '/': { prerender: true }
+  },
+
+  compatibilityDate: '2025-01-15',
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
+})
